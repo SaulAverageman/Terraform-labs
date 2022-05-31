9a1f8a471d8c75a2a9ee55214d9a3ae0bf178a52
bubaiTLE <86016590+bubaiTLE@users.noreply.github.com>
2022-05-31 11:11:29 +0530

    Create main.tf

---
+++ database/main.tf
@@ -0,0 +1,21 @@
+terraform {
+ required_providers {
+  aws = {
+   source = "hashicorp/aws"
+   version = "~> 3.0"
+   }
+ }
+}
+
+provider "aws" {
+ region = var.region
+}
+resource "aws_db_instance" "mysqldb" {
+ allocated_storage = var.storage
+ engine = var.engine
+ engine_version = var.engine_version
+ instance_class = var.instance_class
+ name = var.name
+ username = var.username
+ password = var.password
+}
